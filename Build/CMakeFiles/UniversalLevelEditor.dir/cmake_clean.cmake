17c9cc6d40d4e71c35032cff94571542e3581497
RCornejoM <rodrigo.cornejo.m@ug.uchile.cl>
2025-07-31 20:06:54 -0400

    FEAT CMake build

---
+++ Build/CMakeFiles/UniversalLevelEditor.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "/home/rodrigo/Desktop/Projects/UniversalLevelEditor/UniversalLevelEditor"
+  "/home/rodrigo/Desktop/Projects/UniversalLevelEditor/UniversalLevelEditor.pdb"
+  "CMakeFiles/UniversalLevelEditor.dir/config.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/config.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/globals.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/globals.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/gui.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/gui.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_impl_glut.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_impl_glut.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/main.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/main.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/shaders.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/shaders.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/stamp.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/stamp.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/tilemap.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/tilemap.cpp.o.d"
+  "CMakeFiles/UniversalLevelEditor.dir/tileset.cpp.o"
+  "CMakeFiles/UniversalLevelEditor.dir/tileset.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UniversalLevelEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
